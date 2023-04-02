8d28c9fc749a7b99384bbba00ce02bf0caca9de8
muthuselvamump <muthuselvamump@gmail.com>
2023-04-02 17:32:11 +0530

    updated version

---
+++ selectionsort.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+public class selectionsort{
+    public static void main(String[] args){
+        Scanner a=new Scanner(System.in);
+        System.out.println("enter a array length");
+        int len=a.nextInt();
+        int arr[]=new int[len];
+        System.out.println("enter a array value");
+        for(int l=0;l<arr.length;l++){
+            arr[l]=a.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int k=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=k;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
