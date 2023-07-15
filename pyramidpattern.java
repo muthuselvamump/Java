f8b4a84abcaa8ff2dd98069ba54782bd3f31ab87
muthuselvamump <muthuselvamump@gmail.com>
2023-07-15 22:52:04 +0530

    pyramidpattern

---
+++ pyramidpattern.java
@@ -0,0 +1,14 @@
+public class pyramidpattern {
+    public static void main(String [] args){
+        for(int i=0;i<5;i++){
+            for(int k=5;k>i;k--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
