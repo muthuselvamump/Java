3d79296607ec6fa02c83c2580b908745c322a191
muthuselvamump <muthuselvamump@gmail.com>
2023-07-18 21:57:30 +0530

    types of operators

---
+++ arithmaticoperator.java
@@ -0,0 +1,10 @@
+public class arithmaticoperator {
+    public static void main(String[] args){
+        System.out.println(10+2);
+        System.out.println(15-3);
+        System.out.println(14/2);
+        System.out.println(20*2);
+        System.out.println(12%2);
+    }
+    
+}
