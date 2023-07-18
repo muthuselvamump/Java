a941b8c56f8fec43f0e31e6188d4e2501369c9a1
muthuselvamump <muthuselvamump@gmail.com>
2023-07-18 22:00:45 +0530

    leftshift operators

--- leftshiftoperator.java
+++ leftshiftoperator.java
@@ -3,7 +3,7 @@ public class leftshiftoperator {
         System.out.println(10<<2);
         System.out.println(15<<3);
         System.out.println(14<<2);
-        System.out.println(20<<2);
+        System.out.println(25<<2);
     }
     
 } 
