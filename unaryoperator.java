0a548ef0355277836b3c02c83810475225d31349
muthuselvamump <muthuselvamump@gmail.com>
2023-07-19 22:04:43 +0530

    unary

---
+++ unaryoperator.java
@@ -0,0 +1,19 @@
+public class unaryoperator {
+    public static void main(String [] args){
+        int a=10;
+        int b=15;
+        System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(b);
+        int c=10;
+        int d=-12;
+        System.out.println(~c);
+        System.out.println(~d);
+        boolean e=true;
+        boolean f=false;
+        System.out.println(!e);
+        System.out.println(!f);
+
+    }
+    
+}
