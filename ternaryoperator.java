fbe6759163498cdab4553d9a70baea589e36bdc7
muthuselvamump <muthuselvamump@gmail.com>
2023-07-19 22:03:33 +0530

    ternary

---
+++ ternaryoperator.java
@@ -0,0 +1,9 @@
+public class ternaryoperator {
+    public static void main(String [] args){
+        int a=10;
+        int b=8;
+        int val=(a<b)?a:b;
+        System.out.println(val);
+    }
+    
+}
