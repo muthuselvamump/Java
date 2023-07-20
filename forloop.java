e660296a2c17012ab66b77e1ae1a03e87800b98b
muthuselvamump <muthuselvamump@gmail.com>
2023-07-20 22:25:52 +0530

    forloopsample

---
+++ forloop.java
@@ -0,0 +1,9 @@
+public class forloop {
+    public static void main(String [] args){
+        int arr[]={12,5,66,77,45,64,95,95,42,};
+        for(int i:arr){
+            System.out.println(i);
+        }
+    }
+    
+}
