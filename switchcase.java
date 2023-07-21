48833a2112e6d1616f97b547a6292ae772c18c33
muthuselvamump <muthuselvamump@gmail.com>
2023-07-21 22:24:50 +0530

    switchcase

---
+++ switchcase.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class switchcase {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a num 1 to 5");        
+        int num=scan.nextInt();
+        switch(num){
+            case 1:{
+                System.out.println("stunning");
+                break;
+            }
+            case 2:{
+                System.out.println("marvelous");
+                break;
+            }
+            case 3:{
+                System.out.println("good-looking");
+                break;
+            }
+            case 4:{
+                System.out.println("charming");
+                break;
+            }
+            case 5:{
+                System.out.println("gorgeous");
+            }
+
+        }
+
+    }
+    
+}
