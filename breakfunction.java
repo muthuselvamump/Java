e99a8f918d63ce5d30d3ecf38e393c679c08d2ba
muthuselvamump <muthuselvamump@gmail.com>
2023-07-24 21:13:23 +0530

    break

---
+++ breakfunction.java
@@ -0,0 +1,11 @@
+public class breakfunction {
+    public static void main(String [] args){
+        for(int i=0;i<=9;i++){
+            if(i==5){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
