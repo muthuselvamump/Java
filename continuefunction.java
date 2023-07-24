9caa55ec9114483e1c44d117edacc99bce80e7b2
muthuselvamump <muthuselvamump@gmail.com>
2023-07-24 21:12:43 +0530

    continue

---
+++ continuefunction.java
@@ -0,0 +1,11 @@
+public class continuefunction {
+    public static void main(String [] args){
+        for(int i=0;i<=9;i++){
+            if(i==5 | i==6){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+    }
+}
