d338f7ea6fba173e6e4b66eae94ee41081aea60f
muthuselvamump <muthuselvamump@gmail.com>
2023-07-25 23:01:48 +0530

    break

--- breakfunction.java
+++ breakfunction.java
@@ -2,9 +2,10 @@ public class breakfunction {
     public static void main(String [] args){
         loop:
         for(int i=0;i<=9;i++){
+            inner_loop:
             for(int j=0;j<=9;j++){
             if(i==5){
-                break loop;
+                break inner_loop;
             }
             System.out.println(j);}
         }
