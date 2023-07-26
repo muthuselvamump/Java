10f42ee615459ecafd7df8facdc901499a34f750
muthuselvamump <muthuselvamump@gmail.com>
2023-07-26 21:50:24 +0530

    string

---
+++ stringsample.java
@@ -0,0 +1,8 @@
+public class stringsample{
+    public static void main(String [] args){
+        char[] ch={'h','e','l','l','o'};
+        String sh=new String(ch);
+        System.out.println(sh);
+
+     }
+}
