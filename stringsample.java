9b0c7979fcadbf0183f00c0a704b77160fa73fe8
muthuselvamump <muthuselvamump@gmail.com>
2023-07-26 21:54:40 +0530

    string

--- stringsample.java
+++ stringsample.java
@@ -3,6 +3,12 @@ public class stringsample{
         char[] ch={'h','e','l','l','o'};
         String sh=new String(ch);
         System.out.println(sh);
+        String new1="java";
+        String new2="java";
+        String new4=new String("welcome");
+        String new3=new String("welcome");
 
+        System.out.println(new1 == new2);
+        System.out.println(new4 == new3);
      }
 }
