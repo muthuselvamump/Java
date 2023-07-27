7f924578d391357f47e8bdb490e74f55d5e37e25
muthuselvamump <muthuselvamump@gmail.com>
2023-07-27 20:49:17 +0530

    concatenation

---
+++ concat.java
@@ -0,0 +1,11 @@
+public class concat {
+    public static void main(String args[]){
+        String s1="sachin";
+        String s2="Tendulkar";
+        s1=s1.concat(s2);
+        System.out.println(s1);
+        s2=s1+100+100;
+        System.out.println(s2);
+    }
+    
+}
