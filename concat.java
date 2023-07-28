25c001c1d6a2420a32b5db30b2db352595473c6f
muthuselvamump <muthuselvamump@gmail.com>
2023-07-28 12:51:26 +0530

    concatenation

--- concat.java
+++ concat.java
@@ -6,6 +6,13 @@ public class concat {
         System.out.println(s1);
         s2=s1+100+100;
         System.out.println(s2);
+        StringBuilder s3=new StringBuilder("hello");
+        StringBuilder s4=new StringBuilder( "world");
+        StringBuilder s=s3.append(s4);
+        System.out.println(s);
+        System.out.println(s.toString());
+        
+
     }
     
 }
