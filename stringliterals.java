201af18eb01f5faeac73e9a693d89a80b3b88ef8
muthuselvamump <muthuselvamump@gmail.com>
2023-07-28 12:56:51 +0530

    string

--- stringliterals.java
+++ stringliterals.java
@@ -4,6 +4,13 @@ public class stringliterals {
         String test1=new String("java");
         System.out.println(test.equals(test1));
         System.out.println(test.equalsIgnoreCase(test1));
+        String s1="heloworld";
+        String s2="HELLOWORLD";
+        String s3="HeLlOwOrLd";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s2.toLowerCase());
+        System.out.println(s3.toLowerCase());
+        System.out.println(s3.toUpperCase());
     }
     
 }
