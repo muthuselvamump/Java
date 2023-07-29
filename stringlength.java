bf5e69afc28fdcc2c3d308ad806632b21a05a12c
muthuselvamump <muthuselvamump@gmail.com>
2023-07-29 20:20:22 +0530

    string

--- stringlength.java
+++ stringlength.java
@@ -2,6 +2,12 @@ public class stringlength{
     public static void main(String []args){
         String s="amazonwebservices";
         System.out.println(s.length());
+        if(s.length() < 20){
+            System.out.println("stringlength is less then 20");
+        }
+        else{
+            System.out.println("string length is greater then 20 ");
+        }
     }
     
 }
