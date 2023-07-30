29302a2d9ccba033612b6f4cb740edda31f741e3
muthuselvamump <muthuselvamump@gmail.com>
2023-07-30 14:44:14 +0530

    operators

---
+++ shortcircuitlogicaloperator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class shortcircuitlogicaloperator {
+    public static void main(String[] args) {
+  
+      /*
+      
+      &&
+      ||
+
+      */
+      boolean a=true;
+      boolean b=false;
+      int c=12;
+      
+      System.out.println(a && b);
+      System.out.println(a || b);
+      System.out.println(c>20 && c<30);
+      System.out.println(c>20 || c<30);
+      
+      
+  }
+}
