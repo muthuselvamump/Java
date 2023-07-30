9ede2ca0b5ccfedfe5f4836fe4c3f84835595a70
muthuselvamump <muthuselvamump@gmail.com>
2023-07-30 14:45:13 +0530

    operators

---
+++ Booleanoperator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Booleanoperator {
+    public static void main(String[] args) {
+      /*
+      & and operator
+      | or operator
+      ^ exor operator
+      
+      
+      */
+      
+      boolean a=true;
+      boolean b=false;
+      
+      System.out.println(a&b);
+      System.out.println(a|b);
+      System.out.println(a^b);
+      
+      
+  }
+}
