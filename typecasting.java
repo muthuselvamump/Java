7f6cab611cde3cedddd669fae5ffae03c334479f
muthuselvamump <muthuselvamump@gmail.com>
2023-07-30 19:20:39 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,11 @@
+public class typecasting {
+    public static void main(String args[]){
+        int a=102;
+        long b=a;// widening typecast
+        System.out.println(b);
+        long c=100000;
+        int d=(int)c;//explicit typecast
+        System.out.println(d);
+    }
+    
+}
