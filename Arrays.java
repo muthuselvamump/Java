b017da6ec31778e5e463aa12670d4b1a36af2433
muthuselvamump <muthuselvamump@gmail.com>
2023-08-01 21:36:15 +0530

    arrays

--- Arrays.java
+++ Arrays.java
@@ -6,5 +6,12 @@ public class Arrays {
         arr[1]=20;
         arr[2]=10;
         System.out.println(arr[1]);
+        int arr3[]={10,20,25,30};
+        System.out.println(arr3[3]);
+        int arr4 [][]={{0,1,5,4},{2,5,6,7}};
+        System.out.println(arr4[0][1]);
+        String arr5[]={"java","aws","shell","linux","html","css","javascript"};
+        System.out.println(arr5[6]);
+
     }
 }
