1b5ad833dbafadc2b06fe0285d4389743c3e7c99
muthuselvamump <muthuselvamump@gmail.com>
2023-08-01 21:31:27 +0530

    arrays

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+    public static void main(String []args){
+        int [] arr,arr1,arr2;
+        arr=new int[5];
+        arr[0]=50;
+        arr[1]=20;
+        arr[2]=10;
+        System.out.println(arr[1]);
+    }
+}
