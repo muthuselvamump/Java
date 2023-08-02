335166e55d4d170f964489eca14dc0bb380702ab
muthuselvamump <muthuselvamump@gmail.com>
2023-08-02 19:53:17 +0530

    matrix_multiplication

---
+++ array_matrix_multiplication.java
@@ -0,0 +1,18 @@
+public class array_matrix_multiplication {
+    public static void main(String []args){
+        int arr1[][]={{2,3,5},
+                      {3,4,2},
+                      {2,3,5}};
+        int arr2[][]={{4,4,5},
+                      {5,6,4},
+                      {4,1,3}};
+        int arr3[][][]=new int[3][3][3];
+        for(int i=0;i<arr1.length;i++){
+            for (int j=0;i<arr1.length;i++){
+                arr3[i][j]=
+            }
+        }
+        
+    }
+    
+}
