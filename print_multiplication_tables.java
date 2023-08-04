b12716052ac90a287587e9e8e6d381830241646c
muthuselvamump <muthuselvamump@gmail.com>
2023-08-04 22:53:24 +0530

    multiplication

--- print_multiplication_tables.java
+++ print_multiplication_tables.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class print_multiplication_tables{
     public static void main(String[] args){
         Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=scan.nextInt();
+        System.out.println(num + "th table");
+        scan.nextLine();
+        for(int i=1;i<=10;i++){
+            System.out.println(i + "x" + num + "=" + "" + i*num);
+        }
     }
 }
