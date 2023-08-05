ea8d22a6376abfbfac9016edd29daf472edb13fa
muthuselvamump <muthuselvamump@gmail.com>
2023-08-05 23:21:28 +0530

    for_each_loop

---
+++ for_each_loop.java
@@ -0,0 +1,11 @@
+public class for_each_loop {
+    public static void main(String [] args){
+        int arr[]={10,14,5,8,45,78,65,55};
+
+    for(int a:arr){
+        System.out.println(a);
+    }
+
+    }
+    
+}
