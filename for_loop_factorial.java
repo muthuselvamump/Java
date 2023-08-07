2cdd9a9d86b693c0caa617b5b38507cb3dccc16b
muthuselvamump <muthuselvamump@gmail.com>
2023-08-07 20:36:19 +0530

    for_loop_factorial

--- for_loop_factorial.java
+++ for_loop_factorial.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class for_loop_factorial {
     public static void main(String [] args){
         Scanner scan=new Scanner(System.in);
-        System.out.println("Enter a number"); //Using a double you'll get up to 170!
+        System.out.println("Enter a number");
         int num=scan.nextInt();
         BigInteger n = BigInteger.valueOf(1);
         for(int i=1;i<=num;i++){
