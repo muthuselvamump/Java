5e8375553181b8c2bf68e35ca25b260e36bc34b7
muthuselvamump <muthuselvamump@gmail.com>
2023-08-10 23:35:50 +0530

    swap_the_value_without_third_variable

--- swap_the_value_without_third_variable.java
+++ swap_the_value_without_third_variable.java
@@ -8,6 +8,14 @@ public class swap_the_value_without_third_variable {
         a=a^b;
         System.out.println(a);
         System.out.println(b);
+        int c=10;
+        int d=19;
+        c=c+d;
+        d=c-d;
+        c=c-d;
+        System.out.println(c);
+        System.out.println(d);
+
     }
     
 }
