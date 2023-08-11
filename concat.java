f5f90436078b591e20b222685433f1a7d74c0537
muthuselvamump <muthuselvamump@gmail.com>
2023-08-11 22:52:33 +0530

    concatenation

--- concat.java
+++ concat.java
@@ -3,6 +3,7 @@ public class concat {
         String s1="sachin";
         String s2="Tendulkar";
         s1=s1.concat(s2);
+        String s6="java"+"OOPs";
         System.out.println(s1);
         s2=s1+100+100;
         System.out.println(s2);
@@ -11,6 +12,7 @@ public class concat {
         StringBuilder s5=s3.append(s4);
         System.out.println(s5);
         System.out.println(s5.toString());
+        System.out.println(s6);
         
 
     }
