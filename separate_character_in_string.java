b9bf81051c31a78a2b67b65ba3242de9d460e608
muthuselvamump <muthuselvamump@gmail.com>
2023-08-12 12:22:28 +0530

    separate_character_in_string

--- separate_character_in_string.java
+++ separate_character_in_string.java
@@ -1,7 +1,11 @@
 public class separate_character_in_string {
     public static void main(String[] args ){
         String s="Helloworld";
-        
+        for(int i=0;i<s.length();i++){
+            System.out.println(s.charAt(i));
+
+        }
+
     }
     
 }
