d7ba7f78ea10f15d3b5904795d950208ae872bfb
muthuselvamump <muthuselvamump@gmail.com>
2023-08-28 23:30:08 +0530

    immutable_String_vs_StringBuffer_vs_StringBuilder

--- immutable_String_vs_StringBuffer_vs_StringBuilder.java
+++ immutable_String_vs_StringBuffer_vs_StringBuilder.java
@@ -3,6 +3,13 @@ public class immutable_String_vs_StringBuffer_vs_StringBuilder {
         String s1="Hello";
         s1.concat("World");
         System.out.println(s1);
+        StringBuffer s2=new StringBuffer("Hello");
+        s2.append("World");
+        System.out.println(s2);
+        StringBuilder s3=new StringBuilder("Hello");
+        s3.append("World");
+        System.out.println(s2);
+
 
     }
     
