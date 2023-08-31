34110620e1b368297e2ecda3f64a2f67521f9b9b
muthuselvamump <muthuselvamump@gmail.com>
2023-08-31 23:22:23 +0530

    decode_output

---
+++ decode_output.java
@@ -0,0 +1,11 @@
+public class decode_output {
+    public static void main(String []args){
+        for(int i=1;i<4;i++){
+            if(i==4){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
