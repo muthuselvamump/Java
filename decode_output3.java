a0abcd2a5b72765c9b12da436cb959a5d330c8a8
muthuselvamump <muthuselvamump@gmail.com>
2023-09-01 23:28:49 +0530

    decode_output

--- decode_output3.java
+++ decode_output3.java
@@ -1,6 +1,8 @@
 public class decode_output3 {
     public static void main(String []args){
         //\u000d System.out.println("javaquiz");
+        
+        /* \u000d System.out.println("javaquiz1");*/
     }
     
 }
