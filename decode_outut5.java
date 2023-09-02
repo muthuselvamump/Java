7146a2485efb4090c35b9464c74e2dbef0b4ed65
muthuselvamump <muthuselvamump@gmail.com>
2023-09-02 23:43:04 +0530

    decode_output

---
+++ decode_outut5.java
@@ -0,0 +1,11 @@
+class A{
+    static int a=80;
+    int b=20;
+}
+public class decode_outut5 {
+    public static void main(String[] args){
+        A obj=null;
+        System.out.println(obj.a);
+    }
+    
+}
