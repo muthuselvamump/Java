271777afbe9cda04408712a2d1d44e019c3f0402
muthuselvamump <muthuselvamump@gmail.com>
2023-09-25 22:46:29 +0530

    vowels_to_55_String.java

---
+++ vowels_to_55_String.java
@@ -0,0 +1,7 @@
+public class vowels_to_55_String {
+    public static void main(String []args){
+        String s="helloworld";
+        StringBuilder str=new StringBuilder(s);
+        if()
+    }    
+}
