d4b3ddb682b13d3bbc87c5d4dd98ba2503544f20
muthuselvamump <muthuselvamump@gmail.com>
2023-09-25 22:53:20 +0530

    vowels_to_55_String

--- vowels_to_55_String.java
+++ vowels_to_55_String.java
@@ -2,6 +2,12 @@ public class vowels_to_55_String {
     public static void main(String []args){
         String s="helloworld";
         StringBuilder str=new StringBuilder(s);
-        if()
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
+                str.setCharAt(i,'5');
+            }
+        }
+        s=str.toString();
+        System.out.println(s);
     }    
 }
