06fff56cbbfed7c39b4e3fcca68a617bcf4957ae
muthuselvamump <muthuselvamump@gmail.com>
2023-10-08 21:28:23 +0530

    String_methods

--- String_methods.java
+++ String_methods.java
@@ -1,6 +1,8 @@
 public class String_methods {
     public static void main(String []args){
         String s="helloworld";
+        s=s.replaceAll("[e,l,w]", "h");
+        System.out.println(s);
     }
     
 }
