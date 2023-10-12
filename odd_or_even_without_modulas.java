c3386413ab41aeeb3dcf42d22c3ea293b046c94c
muthuselvamump <muthuselvamump@gmail.com>
2023-10-12 22:41:19 +0530

    odd_or_even_without_modulas

--- odd_or_even_without_modulas.java
+++ odd_or_even_without_modulas.java
@@ -1,7 +1,7 @@
 public class odd_or_even_without_modulas {
     public static boolean iseven(int n){
         boolean iseven=true;
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<n;i++){
             iseven=!iseven;
         }
         return iseven;
@@ -9,8 +9,12 @@ public class odd_or_even_without_modulas {
 
     }
     public static void main(String []args){
+        int n=50;
         if(iseven(n)){
-            System.out.println()
+            System.out.println("Even number");
+        }
+        else{
+            System.out.printf("odd number");
         }
 
     }
