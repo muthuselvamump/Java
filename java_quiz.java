4927d82c2ff257f7b570e051f158c1708b2ca965
muthuselvamump <muthuselvamump@gmail.com>
2023-10-13 23:03:37 +0530

    java_quiz

--- java_quiz.java
+++ java_quiz.java
@@ -1,14 +1,19 @@
 public class java_quiz {
     public int i=1;
+    static int k=2;
     int data=10;
     static public void main(String []args){
         int i=2;
         java_quiz new1=new java_quiz();
         new1.method2();
+        method3();
 
 
     }
     public void method2(){
         System.out.println(i);
+    }
+    public static void method3(){
+        System.out.println(k);
     }
 }
