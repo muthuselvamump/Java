a17176b912ea9bf99a4e3be2d94e431a54bfc533
muthuselvamump <muthuselvamump@gmail.com>
2023-10-18 21:31:43 +0530

    count_how_many_character_in_a_string

---
+++ count_how_many_character_in_a_string.java
@@ -0,0 +1,9 @@
+public class count_how_many_character_in_a_string {
+    public static void main(String []args){
+        String s1="helloworld";
+        for(int i=0;i<s1.length();i++){
+            
+        }
+    }
+    
+}
