1d858c2bcbdf35455552c4e6999fb6a55bbb12f0
muthuselvamump <muthuselvamump@gmail.com>
2023-10-18 21:44:35 +0530

    count_how_many_character_in_a_string

--- count_how_many_character_in_a_string.java
+++ count_how_many_character_in_a_string.java
@@ -2,5 +2,13 @@ public class count_how_many_character_in_a_string {
     public static void main(String []args){
         String s1="helloworld";
         System.out.println(s1.length());
+        String s2=" hello java devloper ";
+        int count=0;
+        for(int i=0;i<s2.length();i++){
+            if(s2.charAt(i) != ' '){
+                count++;
+            }
+        }
+        System.out.println("character count except space :- " + count);
     }  
 }
