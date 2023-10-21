ccac5f0a248e174c08688517f26910ee0911bab3
muthuselvamump <muthuselvamump@gmail.com>
2023-10-21 19:12:19 +0530

    add_number_without_addition_operator

--- add_number_without_addition_operator.java
+++ add_number_without_addition_operator.java
@@ -3,6 +3,11 @@ public class add_number_without_addition_operator{
         int a=10;
         int b=15;
         System.out.println((a-(-b)));
+        int x=10;
+        int y=10;
+        String str = String.format("%" + x + "c%" + y + "c", ' ', ' ');
+       // System.out.println(str);
+        System.out.println(str.length());
     }
 
 }
