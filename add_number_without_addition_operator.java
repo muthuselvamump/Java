15e22a168424d120352cf1b0c3623c5da4749d68
muthuselvamump <muthuselvamump@gmail.com>
2023-10-21 18:46:12 +0530

    add_number_without_addition_operator

---
+++ add_number_without_addition_operator.java
@@ -0,0 +1,8 @@
+public class add_number_without_addition_operator{
+    public static void main(String []args){
+        int a=10;
+        int b=15;
+        System.out.println((a-(-b)));
+    }
+
+}
