fef14c65de380b7c1e6389e50d03cd28bcc5fe70
muthuselvamump <muthuselvamump@gmail.com>
2023-10-22 22:47:55 +0530

    number_of_digit_in_integer

---
+++ number_of_digit_in_integer.java
@@ -0,0 +1,9 @@
+public class number_of_digit_in_integer {
+    public static void main(String []args){
+        int number=15244;
+        int length = String.valueOf(number).length();
+        System.out.println(length);
+        int length1=
+    }
+    
+}
