b617495704f0b1b1d3a28d56617509a1aa39d860
muthuselvamump <muthuselvamump@gmail.com>
2023-10-22 22:50:20 +0530

    number_of_digit_in_integer

--- number_of_digit_in_integer.java
+++ number_of_digit_in_integer.java
@@ -3,7 +3,8 @@ public class number_of_digit_in_integer {
         int number=15244;
         int length = String.valueOf(number).length();
         System.out.println(length);
-        int length1=
+        int length1=(int)Math.log10(number)+1;
+        System.out.println(length1);
     }
     
 }
