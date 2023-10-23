941c3271b7b2d5b5e3f77c8563e51db8392154d2
muthuselvamump <muthuselvamump@gmail.com>
2023-10-23 21:01:54 +0530

    quiz2

---
+++ quiz2.java
@@ -0,0 +1,8 @@
+public class quiz2 {
+    public static void main(String []args){
+        StringBuffer s1=new StringBuffer("Quiz");
+        StringBuffer s2=s1.reverse();
+        System.out.println(s2);
+    }
+    
+}
