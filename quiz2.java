e9f5b19b7644e6a7077db1a3565526d0704e03aa
muthuselvamump <muthuselvamump@gmail.com>
2023-10-23 21:04:19 +0530

    quiz2

--- quiz2.java
+++ quiz2.java
@@ -3,6 +3,9 @@ public class quiz2 {
         StringBuffer s1=new StringBuffer("Quiz");
         StringBuffer s2=s1.reverse();
         System.out.println(s2);
+        String s3="java:"+5+5+5;
+        System.out.println(s3);
+
     }
     
 }
