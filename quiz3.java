9b30ec81be292d333a09f0c133c366104eff0fe1
muthuselvamump <muthuselvamump@gmail.com>
2023-10-25 21:36:55 +0530

    quiz3

--- quiz3.java
+++ quiz3.java
@@ -3,11 +3,10 @@ public class quiz3{
         int []arr={1,2,3,4,5};
         System.out.println(findvalue(arr));
         String s1="hello";
-        s1.replace("{e,l,l}", "h");
+        s1.replaceAll("[e,l,l]", "h");
         System.out.println(s1);
-
-
-
+        s1=s1.replaceAll("[e,l,l]", "");
+        System.out.println(s1);
 
     }
     public static int findvalue(int []ar1){
