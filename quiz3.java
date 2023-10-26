b76b5f70f4f437c2e453548911462404654c6072
muthuselvamump <muthuselvamump@gmail.com>
2023-10-26 20:23:14 +0530

    quiz3

--- quiz3.java
+++ quiz3.java
@@ -1,4 +1,5 @@
 public class quiz3{
+    int k=0;
     public static void main(String []args){
         int []arr={1,2,3,4,5};
         System.out.println(findvalue(arr));
@@ -7,7 +8,7 @@ public class quiz3{
         System.out.println(s1);
         s1=s1.replaceAll("[e,l,l]", "");
         System.out.println(s1);
-
+        System.out.println('j' + 'a' + 'v' + 'a');//ASCII value of code 106 + 97 + 118 + 97 = 418
     }
     public static int findvalue(int []ar1){
         return ar1[ar1.length-ar1[2]];
