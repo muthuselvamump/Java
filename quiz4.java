1f66a2c253abfa625454cb5e723d97982c87f717
muthuselvamump <muthuselvamump@gmail.com>
2023-11-08 22:22:00 +0530

    quiz4

--- quiz4.java
+++ quiz4.java
@@ -1,5 +1,10 @@
 public class quiz4 {
     public static void main(String []args){
         byte x=127;
+        x++;
+        x++;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
     }
 }
