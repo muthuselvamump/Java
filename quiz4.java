729c290f06406c1369aeb8a42b7d4d29071265a6
muthuselvamump <muthuselvamump@gmail.com>
2023-11-09 22:22:02 +0530

    quiz4

--- quiz4.java
+++ quiz4.java
@@ -3,8 +3,11 @@ public class quiz4 {
         byte x=127;
         x++;
         x++;
-        System.out.println(x);
+        System.out.println(x);//range -127 to 127
         x++;
         System.out.println(x);
+        int y=2147483647;
+        y++;
+        System.out.println(y);
     }
 }
