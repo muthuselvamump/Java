0452734984b701b0aff2edfb67ee40987a6ae14c
muthuselvamump <muthuselvamump@gmail.com>
2023-11-10 20:54:44 +0530

    quiz5

--- quiz5.java
+++ quiz5.java
@@ -6,7 +6,8 @@ StringBuilder sb1 = new StringBuilder();
 sb1.append("Ja").append("va");  
 System.out.println(s1 == s2);  
 System.out.println(s1.equals(s2));  
- 
+System.out.println(sb1.toString() == s1);  
+System.out.println(sb1.toString().equals(s1));  
     }
     
 }
