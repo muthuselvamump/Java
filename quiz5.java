bd81aaf64b89f562c25d5da3397e1ccb8b2c377c
muthuselvamump <muthuselvamump@gmail.com>
2023-11-10 20:52:19 +0530

    quiz5

---
+++ quiz5.java
@@ -0,0 +1,12 @@
+public class quiz5 {
+    public static void main(String []args){
+        String s1 = "Java";  
+String s2 = "Java";  
+StringBuilder sb1 = new StringBuilder();  
+sb1.append("Ja").append("va");  
+System.out.println(s1 == s2);  
+System.out.println(s1.equals(s2));  
+ 
+    }
+    
+}
