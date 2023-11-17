8bd6872520a2fab89bc46482fe806d3fd5c7380f
muthuselvamump <muthuselvamump@gmail.com>
2023-11-17 11:29:51 +0530

    quiz5

--- quiz5.java
+++ quiz5.java
@@ -7,7 +7,10 @@ sb1.append("Ja").append("va");
 System.out.println(s1 == s2);  
 System.out.println(s1.equals(s2));  
 System.out.println(sb1.toString() == s1);  
-System.out.println(sb1.toString().equals(s1));  
+System.out.println(sb1.toString().equals(s1));
+int five=5;
+int two=2;
+int total 
     }
     
 }
