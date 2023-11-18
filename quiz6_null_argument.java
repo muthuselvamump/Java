ad25d9d7d6cfa5b6ab38d0195d6e3b39a8f63e4a
muthuselvamump <muthuselvamump@gmail.com>
2023-11-18 22:12:25 +0530

    quiz6_null_argument

---
+++ quiz6_null_argument.java
@@ -0,0 +1,11 @@
+public class quiz6_null_argument{
+    public static void main(String []args){
+        foo(null);
+
+    }  
+    public static void foo(Object obj){
+        System.out.println("Object argument");
+    }
+    
+
+}
