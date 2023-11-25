b6a48742bc6118ff749b2cd8e33635fff1c879e0
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-11-25 21:06:53 +0530

    Add files via upload

---
+++ quiz15.java
@@ -0,0 +1,13 @@
+public class quiz15 {
+    public static void main(String[] args) {
+        Integer num1 = 100;
+        Integer num2 = 100;
+        if (num1 == num2) {
+            System.out.println("num1 == num2");
+        } else {
+            System.out.println("num1 != num2");
+        }
+
+    }
+
+}
