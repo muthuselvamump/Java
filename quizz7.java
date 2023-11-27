b87f56fa67d43be48821809b314780061b6237e0
muthuselvamump <muthuselvamump@gmail.com>
2023-11-27 22:34:57 +0530

    quizz7

--- quizz7.java
+++ quizz7.java
@@ -12,6 +12,9 @@ public class quizz7 {
         Integer i=100;
         Integer i1=100;
         System.out.println(i==i1);
+        Integer i2=128;
+        Integer i3=128;
+        System.out.println(i2==i3);//By default, these values will be from -128 to 127. So, for any number you pass between -128 and 127 (inclusive), it doesn't create the new Integer object. Instead it returns already cached object from the array. If you pass the number beyond this range, then only it creates a new Integer object
         
     }
     
