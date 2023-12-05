0485a07641f7a092aab59b3adcf0f304fee71058
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-12-05 13:23:07 +0530

    reverse_string

--- reverse_string.java
+++ reverse_string.java
@@ -1,6 +1,10 @@
+import java.util.*;
 public class reverse_string{
     public static void main(String[] args) {
-        String str="java";
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Please Enter a String :");
+        String str=scan.nextLine();
+        //String str="java";
         String str1="";
         for(int i=str.length()-1;i>=0;i--){
             str1 +=str.charAt(i);
