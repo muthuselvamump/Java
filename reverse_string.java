9136b737c4fca35497260b67c5eee9aaf71c1309
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2023-12-06 10:46:51 +0530

    reverse a string in array

--- reverse_string.java
+++ reverse_string.java
@@ -2,13 +2,25 @@ import java.util.*;
 public class reverse_string{
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        System.out.println("Please Enter a String :");
-        String str=scan.nextLine();
+        System.out.println("Please Enter a array length: ");
+        int n=scan.nextInt();
+        scan.nextLine();
+        String str2[]=new String[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Please Enter a String: ");
+            str2[i]=scan.nextLine();
+        }
+        
+        /*String str=scan.nextLine();
         //String str="java";
         String str1="";
         for(int i=str.length()-1;i>=0;i--){
             str1 +=str.charAt(i);
         }
-        System.out.println(str1.toString());
+        System.out.println(str1);*/
+       // System.out.println(str2.toString());
+        for(String s:str2){
+            System.out.println(s);
+        }
     }
 }
