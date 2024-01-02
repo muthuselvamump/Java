215119338143b26c1fa59175f959583312b733ed
muthuselvamump <muthuselvamump@gmail.com>
2024-01-02 21:31:38 +0530

    quiz_18

---
+++ quiz_18.java
@@ -0,0 +1,10 @@
+public class quiz_18 {
+    public static void main(String []args){
+       int i=15;
+        {
+            i=55;
+        }
+        System.out.println(i);
+        System.out.println(5+"5"+5);
+    }
+}
