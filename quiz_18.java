489230b00258b84142ee4169f8265c67291994d1
muthuselvamump <muthuselvamump@gmail.com>
2024-01-02 21:37:08 +0530

    quiz_18

--- quiz_18.java
+++ quiz_18.java
@@ -1,10 +1,14 @@
 public class quiz_18 {
+    private static int $=33;
     public static void main(String []args){
        int i=15;
+       int $=45;
+       
         {
             i=55;
         }
         System.out.println(i);
         System.out.println(5+"5"+5);
+        System.out.println($);
     }
 }
