b923d8ce5de19f30c2d929099972c60628cb621d
muthuselvamump <muthuselvamump@gmail.com>
2024-01-09 21:40:46 +0530

    quiz_18

--- quiz_18.java
+++ quiz_18.java
@@ -1,5 +1,6 @@
 public class quiz_18 {
     private static int $=33;
+    int the=55;
     public static void main(String []args){
        int i=15;
        int $=45;
@@ -10,5 +11,7 @@ public class quiz_18 {
         System.out.println(i);
         System.out.println(5+"5"+5);
         System.out.println($);
+        System.out.println(quiz_18.$);
+        
     }
 }
