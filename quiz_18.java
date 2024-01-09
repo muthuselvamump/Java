0f1cf24bc7db946ffed5d86c097482c787253d4a
muthuselvamump <muthuselvamump@gmail.com>
2024-01-09 21:41:16 +0530

    quiz_18

--- quiz_18.java
+++ quiz_18.java
@@ -12,6 +12,10 @@ public class quiz_18 {
         System.out.println(5+"5"+5);
         System.out.println($);
         System.out.println(quiz_18.$);
+        int the=66;
+        System.out.println(the);
+        quiz_18 new1=new quiz_18();
+        System.out.println(new1.the);
         
     }
 }
