5197dec1c167429107094e612742ee3a5bb74686
muthuselvamump <muthuselvamump@gmail.com>
2024-01-10 22:11:16 +0530

    quiz19

--- quiz19.java
+++ quiz19.java
@@ -4,6 +4,11 @@ public class quiz19 {
         String y = "abc";
         x = x + y;//four string object created
         System.out.println(x);
+        String x1= "abc";
+        x1.toUpperCase(); 
+        String y1 = x1.replace('A', 'a');
+        y1 = y1 + "hjk";
+        System.out.println(y1);
     }
     
 }
