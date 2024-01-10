203ee479e788c80ac168e36186dd9400324f27c0
muthuselvamump <muthuselvamump@gmail.com>
2024-01-10 22:07:39 +0530

    quiz19

---
+++ quiz19.java
@@ -0,0 +1,9 @@
+public class quiz19 {
+    public static void main(String []args){
+        String x = new String("xyz");
+        String y = "abc";
+        x = x + y;//four string object created
+        System.out.println(x);
+    }
+    
+}
