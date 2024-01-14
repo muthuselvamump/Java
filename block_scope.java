5cbbdfae0dad9a7c72f788a857d69d9b6b05de02
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-01-14 18:31:43 +0530

    blockscope

---
+++ block_scope.java
@@ -0,0 +1,14 @@
+public class block_scope {
+    public static void main(String []args){
+        {
+            int a=177;
+
+            System.out.println(a);
+            
+        } //block end cant use a
+
+
+        //System.out.println(a);
+    }
+    
+}
