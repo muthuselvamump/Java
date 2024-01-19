c7c96f2184a0054edbd59fbaf48556e5ee65585d
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-01-19 19:12:47 +0530

    Add files via upload

---
+++ quizz23.java
@@ -0,0 +1,13 @@
+public class quizz23 {
+    public static void main(String []args){
+        double d=0.0f;
+        int i=5;
+        //int i1=i/(int)d;//its throw arithmaetic exception
+       // System.out.println(i1);
+        double d1=0.00001;
+        double d2=5;
+        double d3=d2/d1;
+        System.out.println(d3);
+    }
+    
+}
