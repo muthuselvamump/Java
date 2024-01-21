0c03134a56cdfcb4768b9f02e05b499cef4fc8bd
muthuselvamump <muthuselvamump@gmail.com>
2024-01-21 21:48:16 +0530

    quiz23

---
+++ quiz23.java
@@ -0,0 +1,13 @@
+public class quiz23 {
+    public static void main(String args[]){
+        int result = 0;
+        quiz23 oc = new quiz23();
+        Object o = oc;
+
+        if (o == oc)  
+            result = 1;
+        if (o != oc)  
+            result = result + 10;
+      
+    
+}
