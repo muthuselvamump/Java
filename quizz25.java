772f2f6bb56eed936ff18dc3a0c3ba25aa1fefb8
muthuselvamump <muthuselvamump@gmail.com>
2024-02-01 23:38:30 +0530

    quiz

--- quizz25.java
+++ quizz25.java
@@ -2,11 +2,12 @@ public class quizz25 {
     public static void main(String[] args) 
     { 
         int $_ = 5; 
-        System.out.println($_);
+        System.out.println($_);//special charator allowed for variable declarartion
 
         int x=10;
         if(x==10){
-         
+            int x=8;
+            System.out.println(x);// dublicate local variable error
         }
     } 
     
