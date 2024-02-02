257d74bb159497c029fe339b725bfa281bf5ee25
muthuselvamump <muthuselvamump@gmail.com>
2024-02-02 21:20:52 +0530

    quiz

--- quizz25.java
+++ quizz25.java
@@ -6,9 +6,13 @@ public class quizz25 {
 
         int x=10;
         if(x==10){
-            int x=8;
+          //  int x=8;
             System.out.println(x);// dublicate local variable error
         }
+        Integer a=Integer.valueOf(33);
+        int b=a+x;
+        System.out.println(b);
+        
     } 
     
 }
