058117ef381680f1c6167c2e75acdd2a9aee4ea6
muthuselvamump <muthuselvamump@gmail.com>
2024-02-03 21:49:28 +0530

    factorial_using_loops

---
+++ factorial_using_loops.java
@@ -0,0 +1,10 @@
+public class factorial_using_loops {
+    public static void main(String []args){
+        int a=5;
+        int b=1;
+        for(int i=1;i<=a;i++){
+            b *=i;
+        }
+        System.out.println(b);
+    }
+}
