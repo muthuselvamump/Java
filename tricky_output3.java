75a8ef87f2b1f01616ef07daa668951d24274a72
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-02-14 20:43:58 +0530

    Add files via upload

---
+++ tricky_output3.java
@@ -0,0 +1,20 @@
+public class tricky_output3 {
+        static int a=01;
+        static Integer A;
+        static boolean b;
+        static double d=0.1423423423424234d;
+        static float f=3.12F;
+        static long l=2;
+        static char c;
+    
+        public static void main(String[] args) {
+            System.out.println("int:"+a);
+            System.out.println("boolean:"+b);
+            System.out.println("double:"+d);
+            System.out.println("float:"+f);
+            System.out.println("long:"+l);
+            System.out.println("char:"+c);
+            System.out.println(c);
+        }
+    }
+    
