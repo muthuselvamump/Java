18f46455bb18ec41887a35ac0790bd29745b3d5b
muthuselvamump <muthuselvamump@gmail.com>
2024-02-23 21:07:04 +0530

    quiz

--- tricky_output4.java
+++ tricky_output4.java
@@ -1,7 +1,14 @@
-public enum tricky_output4{
+public class tricky_output4{
     public static void main(String []args){
-        for(int i=0;i<1;i++){
-            
+        int i=0;
+        for( i=0;i<1;i++){
+            System.out.println(i);
+
+        }
+        System.out.println(i);
+        for( int j=0;j<1;j++){
+            System.out.println(j+'4');
+
         }
     }
 }
