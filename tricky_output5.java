9f675adbff009f2f99b5b059c57bffbff500c568
muthuselvamump <muthuselvamump@gmail.com>
2024-02-24 20:34:22 +0530

    quiz

--- tricky_output5.java
+++ tricky_output5.java
@@ -1,9 +1,10 @@
 public class tricky_output5 {
  public static void main(String []args){
     int []arr={1,2,3,4,5};
-    int num=0;
+    int sum=0;
     for(int num:arr){
-        sum num+sum;
+        sum =num+sum;
     }
- }   
+    System.out.println(sum);
+}
 }
