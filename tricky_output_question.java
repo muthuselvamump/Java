d72dd0969eec9505f57669738d92dad8d39e8ccd
muthuselvamump <muthuselvamump@gmail.com>
2024-02-29 22:15:02 +0530

    quiz

--- tricky_output_question.java
+++ tricky_output_question.java
@@ -1,6 +1,9 @@
 public class tricky_output_question {
     static int a=1;
+    int b=2;
     public static void main(String []args){
-        System.out.println(a);
+        System.out.println(a);//static variable we can call without obj
+        tricky_output_question t=new tricky_output_question();
+        System.out.println(t.b);
     }
 }
