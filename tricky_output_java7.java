7ea235465fd6951c0b0d121d8195ef397b7833fc
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-03-15 22:15:02 +0530

    Add files via upload

---
+++ tricky_output_java7.java
@@ -0,0 +1,8 @@
+public class tricky_output_java7 {
+    public static void main(String[] args) {
+        Object obj = new Integer(3);
+        String s = (String) obj;
+        System.out.println(s);
+
+    }
+}
