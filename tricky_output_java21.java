9b25d4cdab51c04d22270c95dd9937e92ff100c7
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-03-16 23:07:19 +0530

    Add files via upload

---
+++ tricky_output_java21.java
@@ -0,0 +1,20 @@
+import javax.sound.midi.Track;
+
+public class tricky_output_java21 {
+    public void print(Integer i) {
+        System.out.println("Integer");
+    }
+
+    public void print(int i) {
+        System.out.println("int");
+    }
+
+    public void print(long i) {
+        System.out.println("long");
+    }
+
+    public static void main(String args[]) {
+        tricky_output_java21 test = new tricky_output_java21();
+        test.print(10);
+    }
+}
