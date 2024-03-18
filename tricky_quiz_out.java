1e41234097465f529dcfb4b5945fa091a2bd5ad4
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-03-18 22:33:46 +0530

    Add files via upload

--- tricky_quiz_out.java
+++ tricky_quiz_out.java
@@ -1,5 +1,5 @@
-class a{
+class a{//if we give class as public then should be same filename and class name class
     public static void main(String[] args) {
         System.out.println("hello");
-    }
+        }
 }
