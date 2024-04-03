ad4b1c0b8a46310247b9e068bc5af50959d7ce45
muthuselvamump <muthuselvamump@gmail.com>
2024-04-03 22:44:37 +0530

    quiz

---
+++ tricky_out_quiz.java
@@ -0,0 +1,10 @@
+public class tricky_out_quiz {
+    public static void main(String[] args) {
+        if(36==36)
+            System.out.println("java");
+        else
+            System.out.println("python");
+            System.out.println("developer");
+    }
+    
+}
