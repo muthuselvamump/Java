0debe3a16de1b68b45f96bf81d135136d0395f62
muthuselvamump <muthuselvamump@gmail.com>
2024-04-03 22:48:09 +0530

    quiz

--- tricky_out_quiz.java
+++ tricky_out_quiz.java
@@ -5,6 +5,16 @@ public class tricky_out_quiz {
         else
             System.out.println("python");
             System.out.println("developer");
+
+
+            System.out.println("----------------");
+            System.out.println("After braces");
+            System.out.println("-----------------");
+            if(36==36)
+            System.out.println("java");
+        else{
+            System.out.println("python");
+            System.out.println("developer");
+        }
     }
-    
 }
