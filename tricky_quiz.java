dfebb8b7e11d080a6dfa8a5e0600a090ab01356c
muthuselvamump <muthuselvamump@gmail.com>
2024-04-12 23:00:15 +0530

    quiz

--- tricky_quiz.java
+++ tricky_quiz.java
@@ -1,9 +1,8 @@
 public class tricky_quiz{
     public static void main(String []args){
-        String s1="Java";
+        String s1="JAVA";
         String s2="java";
-        boolean b=false;
-        System.out.println(b);
+        System.out.println(s1==s2);
         System.out.println(s1.compareTo(s2));
         System.out.println(s1.equals(s2));
         System.out.println(s1.equalsIgnoreCase(s2));
