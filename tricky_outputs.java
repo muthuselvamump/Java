52709f6490c3083b191853e6e696e62181d23145
muthuselvamump <muthuselvamump@gmail.com>
2024-04-13 19:24:38 +0530

    quiz

---
+++ tricky_outputs.java
@@ -0,0 +1,8 @@
+public class tricky_outputs {
+    public static void main(String[] args) {
+        for(int i=0;i<4;i++)
+        System.out.println("iteration");
+        System.out.println("java");
+    }
+    
+}
