753ce387cee46db8f21a74ce23e6be008edd1bba
muthuselvamump <muthuselvamump@gmail.com>
2024-04-23 23:49:24 +0530

    quiz

--- tricky_question.java
+++ tricky_question.java
@@ -9,6 +9,16 @@ public class tricky_question {
         byte $date=(byte) 2205199;
 
         System.out.println($date);
+        double d1=0.1;
+        double d2=0.2;
+        double d3=d1+d2;
+
+        if(d3==0.3){
+            System.out.println("3.5: " +d3);
+        }
+        else{
+            System.out.println("long: "+d3);//because int+int=int, double+double=long, byte+byte=int, float+int=float, int+int=int
+        }
     }
     
 }
