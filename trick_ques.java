19efbf74174cde60521ae3284ad6270afd2d7b37
muthuselvamump <muthuselvamump@gmail.com>
2024-04-24 23:09:13 +0530

    quiz

---
+++ trick_ques.java
@@ -0,0 +1,9 @@
+public class trick_ques {
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+    static{
+        System.out.println("static block");
+    }
+    
+}
