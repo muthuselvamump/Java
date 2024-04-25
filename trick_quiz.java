3acfdd667a80746fc30de9b18285de7ab24dacb6
muthuselvamump <muthuselvamump@gmail.com>
2024-04-26 00:00:37 +0530

    quiz

--- trick_quiz.java
+++ trick_quiz.java
@@ -1,7 +1,12 @@
 public class trick_quiz {
     public static void main(String []args){
         int []arr=new int[10];
-     for(int )
+     for(int i=0;i<10;i++ ){
+        arr[i]=i;
+        System.out.println(i);
+        ++i;
+        System.out.println("hrllo"+i);
+     }
     }
     
 }
