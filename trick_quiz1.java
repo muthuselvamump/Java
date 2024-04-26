da7b8c2d2238bf89f04a19e8f53405ec84f3821e
muthuselvamump <muthuselvamump@gmail.com>
2024-04-26 17:35:07 +0530

    quiz

--- trick_quiz1.java
+++ trick_quiz1.java
@@ -1,7 +1,7 @@
 public class trick_quiz1 {
     public static void main(String []args){
         boolean b=false;
-        if(b==true)
+        if(b=true)
             for(int i=0;i<2;i++)
             System.out.println(i);
         else{
