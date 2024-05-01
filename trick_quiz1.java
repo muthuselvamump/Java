53605ad68721ec37fe38f9ab214178b22910708d
muthuselvamump <muthuselvamump@gmail.com>
2024-05-01 23:43:50 +0530

    quiz

--- trick_quiz1.java
+++ trick_quiz1.java
@@ -10,7 +10,13 @@ public class trick_quiz1 {
         
         short $27_04_2023=(27-04-2024);
         System.out.println("Date"+$27_04_2023);
-        int date=(21-05-2023);
-        System.out.println(date);
+        
+        int date=(21-05-2023);//its going to subtract value
+        System.out.println("subtract the value 21-05-2023=-2007"+date);
+
+        int d=(78);
+        int e=2;
+        int f=e+d;
+        System.out.println(" adition"+f);
      }
 }
