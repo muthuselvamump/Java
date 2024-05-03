e82cda783606c5726fbc9ef8352b97aec0b4f025
muthuselvamump <muthuselvamump@gmail.com>
2024-05-03 23:41:10 +0530

    quiz

--- trick_quiz2.java
+++ trick_quiz2.java
@@ -1,7 +1,10 @@
+import java.util.*;
 public class trick_quiz2 {
     public static void main(String []args){
         String []lang={"c","c++","python","java"};
-        
+        System.out.println(Arrays.toString(lang));
+        String []lanng1={lang[0],lang[1],lang[2],lang[3]};
+        System.out.println(Arrays.toString(lanng1));
     }
     
 }
