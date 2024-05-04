5900cc291ec781ed52734dfcc6486428d685ca5b
muthuselvamump <muthuselvamump@gmail.com>
2024-05-04 23:52:35 +0530

    quiz

--- trick_quiz2.java
+++ trick_quiz2.java
@@ -6,6 +6,8 @@ public class trick_quiz2 {
         String []lanng1={lang[0],lang[1],lang[2],lang[3]};
         System.out.println(Arrays.toString(lanng1));
         System.out.println('H');
+        int $_hjhg=10;
+        System.out.println($_hjhg);
     }
     
 }
