c896016821c1ebc2cecdd2375bf0fb674a04a273
muthuselvamump <muthuselvamump@gmail.com>
2024-05-05 22:49:06 +0530

    quiz

--- trick_quiz3.java
+++ trick_quiz3.java
@@ -8,7 +8,11 @@ int []args1={8,9,5,4,};
         trick_quiz3 a=new trick_quiz3();
         System.out.println(a.args1.length);
         trick_quiz3 a1=new trick_quiz3();
-        
+        System.out.println("1st "+Arrays.toString(a.args1));
+        System.out.println(Arrays.toString(a1.args1));
+        a.args1[1]=55;
+        System.out.println(Arrays.toString(a1.args1));
+        System.out.println("1st "+Arrays.toString(a.args1));
     }
     
 }
