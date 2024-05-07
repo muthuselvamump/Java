c968ede612b5d39430e0717b389e337aa51c1432
muthuselvamump <muthuselvamump@gmail.com>
2024-05-08 00:03:00 +0530

    quiz

--- trick_quiz4.java
+++ trick_quiz4.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class trick_quiz4 {
     int []args={6,4,5,3,9};
     public static void main(String []args){
-        
+        System.out.println(args.length);
+        System.out.println(Arrays.toString(args));
+        trick_quiz4 n=new trick_quiz4();
+        System.out.println("length is: "+n.args.length);  
+        System.out.println(Arrays.toString(n.args)); 
     }
     
 }
