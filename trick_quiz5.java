eb0f7167e87286f976959b8998d3c586e18f9de9
muthuselvamump <muthuselvamump@gmail.com>
2024-05-09 23:40:19 +0530

    quiz

--- trick_quiz5.java
+++ trick_quiz5.java
@@ -1,7 +1,11 @@
 public class trick_quiz5 {
     public static void main(String[] args) {
         int start,count=0,end=5;
-        for
+        for(start =1;start<=end;start++){
+            count++;
+        }
+        System.out.println(count+" "+start);
+        System.out.println(count+start);
     }
     
 }
