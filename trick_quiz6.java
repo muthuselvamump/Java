eb9915238c8af97ff794c3231dc0b0f22726be2c
muthuselvamump <muthuselvamump@gmail.com>
2024-05-10 23:27:41 +0530

    java

--- trick_quiz6.java
+++ trick_quiz6.java
@@ -4,7 +4,7 @@ class trickquiz6{
     }
 }
 public class trick_quiz6 {
-    trickquiz6 hi=new trickquiz6();
+    trickquiz6 hi=new trickquiz6();//like instance variable
     public static void main(String[] args) {
         System.out.println("hello1");
         trick_quiz6 ne=new trick_quiz6();
