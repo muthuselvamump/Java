1c0a04f255e78ea88c7c5219ca0a322e706d9f84
muthuselvamump <muthuselvamump@gmail.com>
2024-05-10 23:25:02 +0530

    java

---
+++ trick_quiz6.java
@@ -0,0 +1,13 @@
+class trickquiz6{
+    public trickquiz6(){
+        System.out.println("hello");
+    }
+}
+public class trick_quiz6 {
+    trickquiz6 hi=new trickquiz6();
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        trick_quiz6 ne=new trick_quiz6();
+    }
+    
+}
