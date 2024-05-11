0d91ae87d3bd5f1a474c3f62871df94731ae0fc8
muthuselvamump <muthuselvamump@gmail.com>
2024-05-11 21:10:34 +0530

    quiz

---
+++ trick_quiz7.java
@@ -0,0 +1,13 @@
+public class trick_quiz7 {
+    public static void main(String []args){
+        int num=2;
+        for(;;){
+            if(num==2){
+                System.out.println("java");
+                break;
+            }
+            System.out.println("spring");
+        }
+    }
+    
+}
