620aed9700bd585bbb384d25c25de95a3d03f199
muthuselvamump <muthuselvamump@gmail.com>
2024-05-13 22:26:30 +0530

    quiz

---
+++ tricky_quiz8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class tricky_quiz8 {
+    public static void main(String args[]){
+        StringJoiner sj=new StringJoiner(",");
+        sj.add("java");
+        sj.add("developer");
+        sj.add("spring");
+        sj.add("boot");
+        System.out.println(sj);
+    }
+    
+}
