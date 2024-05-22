0918faf75d08c48c529e9ec55d223574d82361b6
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-05-22 21:18:21 +0530

    Add files via upload

---
+++ tricky_quiz_java.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class tricky_quiz_java {
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5};
+        Arrays.fill(arr,6);
+        System.out.println(Arrays.toString(arr));
+    }
+    
+}
