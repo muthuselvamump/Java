4647cf2a98ead4ee764944acf057050078ee228c
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-05-23 21:40:55 +0530

    quiz

--- trick_quiz_java1.java
+++ trick_quiz_java1.java
@@ -3,5 +3,10 @@ public class trick_quiz_java1 {
         int x=7;
         int y=8;
         System.out.println(x<y?x:y);
+        short a=5;
+        short b=6;
+        short total=(short)(a++ + b + a);
+        System.out.println(total);
+        
     }
 }
