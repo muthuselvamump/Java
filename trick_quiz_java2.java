2ed1fb1207065d3d817328d776567bf997121153
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-05-24 21:24:08 +0530

    quiz

--- trick_quiz_java2.java
+++ trick_quiz_java2.java
@@ -1,7 +1,9 @@
 public class trick_quiz_java2 {
     public static void main(String []args){
         String str=null;
+        String str2=1+"null"+"null";
         System.out.println(str);
+        System.out.println(str2);
     }
     
 }
