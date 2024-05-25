4a18806c416caa048afc2ce7acf78d7a8d80e797
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-05-25 21:32:31 +0530

    quiz

--- trick_quiz_java2.java
+++ trick_quiz_java2.java
@@ -5,6 +5,7 @@ public class trick_quiz_java2 {
         System.out.println(str);
         System.out.println(str2);
         String s="javadeveloper";
+        System.out.println(s.substring(3,7));
 
 
     }
