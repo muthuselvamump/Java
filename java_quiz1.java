1ed189c0699c93962b4c3b083f2c5a691ef07f65
muthuselvamump <muthuselvamump@gmail.com>
2024-05-28 23:46:33 +0530

    quiz

--- java_quiz1.java
+++ java_quiz1.java
@@ -4,6 +4,7 @@ public class java_quiz1 {
         //System.out.println(arr[0]);//array index out of bound
         int []arr1=new int[1];
         System.out.println(arr1[0]);
+        String str=10+5+"hello";
     }
     
 }
