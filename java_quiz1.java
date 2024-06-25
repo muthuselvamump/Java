97d40a24bfa3c1b86832918fccb983a760032ae7
muthuselvamump <muthuselvamump@gmail.com>
2024-06-25 23:32:14 +0530

    quiz

--- java_quiz1.java
+++ java_quiz1.java
@@ -16,6 +16,8 @@ public class java_quiz1 {
         System.out.println(10+10+concatnum);
         String s1="HELLO";
         s1.concat("WORLD");
+        s1=s1+"coders";
+        System.out.println(s1);
     }
     
 }
