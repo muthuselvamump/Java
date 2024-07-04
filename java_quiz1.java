fa52d7b390f770b8abaca1d91f24e3c069eb1e7c
muthuselvamump <muthuselvamump@gmail.com>
2024-07-04 23:29:39 +0530

    quiz

--- java_quiz1.java
+++ java_quiz1.java
@@ -21,6 +21,8 @@ public class java_quiz1 {
         int num1=4;
         int num2=++(num1);
         System.out.println(num2);
+        String j1="JAVA";
+        String j2=new String("JAVA");
         
     }
     
