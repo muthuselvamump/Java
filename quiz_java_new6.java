70f070669f0bc66f93c75605dac91fb760131b18
Muthuselvam <129104204+muthuselvamump@users.noreply.github.com>
2024-08-16 22:10:58 +0530

    Quiz

---
+++ quiz_java_new6.java
@@ -0,0 +1,7 @@
+public class quiz_java_new6 {
+    public static void main(String[] args) {
+        String str1="java";
+        String str2=new String("java");
+        System.out.println(str1==str2);
+    }
+}
