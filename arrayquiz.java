fde45fb9b88769d16c84e99592df2bb54bd952d1
muthuselvamump <muthuselvamump@gmail.com>
2024-08-31 18:29:22 +0530

    quiz

---
+++ arrayquiz.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class arrayquiz {
+ public static void main(String args[]){
+    int arr[]={1,2,3,4,5,6};
+    arrchange(arr);
+    System.out.println(Arrays.toString(arr));
+ }  
+ 
+ } 
+}
