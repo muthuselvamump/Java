4d22cacd708d5ff52ce5ab0233ca783b0c8cb52a
muthuselvamump <muthuselvamump@gmail.com>
2024-09-01 22:39:17 +0530

    quiz

--- arrayquiz.java
+++ arrayquiz.java
@@ -4,8 +4,12 @@ public class arrayquiz {
     int arr[]={1,2,3,4,5,6};
     arrchange(arr);
     System.out.println(Arrays.toString(arr));
+    String s1="Java";
+    stringchange(s1);
+    System.out.println(s1);
  }  
  public static void arrchange(int []a){
     a[0]=55;
  } 
+ 
 }
