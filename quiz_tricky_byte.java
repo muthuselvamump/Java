9c81c54a39a2095be47f0f58f1ec40baadfd126b
muthuselvamump <muthuselvamump@gmail.com>
2024-09-07 23:32:32 +0530

    quiz_tricky_byte

--- quiz_tricky_byte.java
+++ quiz_tricky_byte.java
@@ -1,6 +1,9 @@
 public class quiz_tricky_byte {
     public static void main(String[] args) {
-        
+        byte b=126;
+        for(;b>0;b++){
+            System.out.println(b);
+        }
     }
     
 }
