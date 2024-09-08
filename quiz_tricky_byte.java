649e0ad429dc326a60a1af7e668e9552ede0b715
muthuselvamump <muthuselvamump@gmail.com>
2024-09-08 22:50:06 +0530

    quiz_tricky_byte

--- quiz_tricky_byte.java
+++ quiz_tricky_byte.java
@@ -1,9 +1,12 @@
+import java.io.IOException;
+
 public class quiz_tricky_byte {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         byte b=126;
         for(;b>0;b++){
             System.out.println(b);
         }
+        System.out.write("t4teq".getBytes());
     }
     
 }
