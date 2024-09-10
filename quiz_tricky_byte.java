fe06abff8a4a9eff469dfd5895bfd18e25352e04
muthuselvamump <muthuselvamump@gmail.com>
2024-09-10 23:00:59 +0530

    quiz_tricky_byte

--- quiz_tricky_byte.java
+++ quiz_tricky_byte.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 class TEST{
     protected int x,y;
     int h;
+    String str;
 }
 public class quiz_tricky_byte {
     public static void main(String[] args) throws IOException {
@@ -13,9 +14,12 @@ public class quiz_tricky_byte {
         System.out.write("java".getBytes());
 
         System.out.println();
+        String str1;
         TEST T=new TEST();
         System.out.println(T.x + " " +T.y);
         System.out.println(T.h);
+        System.out.println(T.str);
+        //System.out.println(str1);
     }
     
 }
